4a6589e4b9009880dc8dbcdda139d375e608032c
tpltnt <1172976+tpltnt@users.noreply.github.com>
2019-04-13 02:33:07 +0200

    minimal netsend via UDP

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::str::FromStr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
+use std::io::{Result};
+
 /// An incomplete implementation of Pure Data message types.
 /// TODO: implement list, pointer and custom message types
 enum PdMessage {
@@ -21,6 +25,27 @@ impl PdMessage {
 }
 
 
+// easier than an enum (for later matching)
+struct NetSendUdp {
+    target: SocketAddr,
+    socket: UdpSocket,
+}
+
+impl NetSendUdp {
+    /// Create a new instance and set target address.
+    fn new(target: &str) -> crate::NetSendUdp {
+       NetSendUdp {
+           target: SocketAddr::from_str(target).expect("failed to parse target address"),
+	   socket: UdpSocket::bind("0.0.0.0:0").expect("failed to bind host socket"),
+       }
+    }
+
+    /// Send a message to the target.
+    fn send(&self, msg: PdMessage) -> Result<usize> {
+        self.socket.send_to(msg.to_text().as_bytes(), self.target)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -41,5 +66,27 @@ mod test {
     fn generate_bang_message() {
         let msg = PdMessage::Bang;
 	assert_eq!(String::from("bang;"), msg.to_text());
+    }
+
+    #[test]
+    fn create_udp_netsend_test_target() {
+        let target = "127.0.0.1:8989";
+        let ns = NetSendUdp::new(&String::from(target));
+
+	assert_eq!(ns.target.is_ipv4(), true);
+	assert_eq!(ns.target.ip(), IpAddr::V4(Ipv4Addr::new(127,0,0,1)));
+	assert_eq!(ns.target.port(), 8989);
+    }
+
+    #[test]
+    fn send_into_ether() {
+        let msg = PdMessage::Bang;
+	let target = "127.0.0.1:8989";
+        let ns = NetSendUdp::new(&String::from(target));
+	let res = ns.send(msg);
+	match res {
+	    Ok(bsend) => assert_eq!(bsend, 5),
+	    Err(fail) => panic!(fail),
+	}
     }
 }
