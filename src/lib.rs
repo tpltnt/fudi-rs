0eaa50eda1d22b5a55f9dcc34862f3e1edc5c196
tpltnt <1172976+tpltnt@users.noreply.github.com>
2019-04-14 21:18:24 +0200

    cleaned up formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::str::FromStr;
+use std::io::Result;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
-use std::io::{Result};
+use std::str::FromStr;
 
 /// An incomplete implementation of Pure Data message types.
 /// TODO: implement list, pointer and custom message types
@@ -15,15 +15,14 @@ impl PdMessage {
     fn to_text(&self) -> String {
         let mut payload: String;
         match &self {
-	    PdMessage::Float(f) => payload = format!("float {}", f),
-	    PdMessage::Symbol(word) => payload = format!("symbol {}", word),
-	    PdMessage::Bang => payload = String::from("bang"),
-	}
-	payload = format!("{};\n", payload);  // newline not in spec, but in vanilla pd
-	return payload;
+            PdMessage::Float(f) => payload = format!("float {}", f),
+            PdMessage::Symbol(word) => payload = format!("symbol {}", word),
+            PdMessage::Bang => payload = String::from("bang"),
+        }
+        payload = format!("{};\n", payload); // newline not in spec, but in vanilla pd
+        return payload;
     }
 }
-
 
 // easier than an enum (for later matching)
 struct NetSendUdp {
@@ -34,10 +33,10 @@ struct NetSendUdp {
 impl NetSendUdp {
     /// Create a new instance and set target address.
     fn new(target: &str) -> crate::NetSendUdp {
-       NetSendUdp {
-           target: SocketAddr::from_str(target).expect("failed to parse target address"),
-	   socket: UdpSocket::bind("0.0.0.0:0").expect("failed to bind host socket"),
-       }
+        NetSendUdp {
+            target: SocketAddr::from_str(target).expect("failed to parse target address"),
+            socket: UdpSocket::bind("0.0.0.0:0").expect("failed to bind host socket"),
+        }
     }
 
     /// Send a message to the target.
@@ -53,19 +52,19 @@ mod test {
     #[test]
     fn generate_float_message() {
         let msg = PdMessage::Float(2.974);
-	assert_eq!(String::from("float 2.974;\n"), msg.to_text());
+        assert_eq!(String::from("float 2.974;\n"), msg.to_text());
     }
 
     #[test]
     fn generate_symbol_message() {
         let msg = PdMessage::Symbol(String::from("foobar"));
-	assert_eq!(String::from("symbol foobar;\n"), msg.to_text());
+        assert_eq!(String::from("symbol foobar;\n"), msg.to_text());
     }
 
     #[test]
     fn generate_bang_message() {
         let msg = PdMessage::Bang;
-	assert_eq!(String::from("bang;\n"), msg.to_text());
+        assert_eq!(String::from("bang;\n"), msg.to_text());
     }
 
     #[test]
@@ -73,32 +72,32 @@ mod test {
         let target = "127.0.0.1:8989";
         let ns = NetSendUdp::new(&String::from(target));
 
-	assert_eq!(ns.target.is_ipv4(), true);
-	assert_eq!(ns.target.ip(), IpAddr::V4(Ipv4Addr::new(127,0,0,1)));
-	assert_eq!(ns.target.port(), 8989);
+        assert_eq!(ns.target.is_ipv4(), true);
+        assert_eq!(ns.target.ip(), IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
+        assert_eq!(ns.target.port(), 8989);
     }
 
     #[test]
     fn send_bang_into_ether() {
         let msg = PdMessage::Bang;
-	let target = "127.0.0.1:8989";
+        let target = "127.0.0.1:8989";
         let ns = NetSendUdp::new(&String::from(target));
-	let res = ns.send(msg);
-	match res {
-	    Ok(bsend) => assert_eq!(bsend, 6),
-	    Err(fail) => panic!(fail),
-	}
+        let res = ns.send(msg);
+        match res {
+            Ok(bsend) => assert_eq!(bsend, 6),
+            Err(fail) => panic!(fail),
+        }
     }
 
     #[test]
     fn send_float_into_ether() {
         let msg = PdMessage::Float(432.0);
-	let target = "127.0.0.1:8989";
+        let target = "127.0.0.1:8989";
         let ns = NetSendUdp::new(&String::from(target));
-	let res = ns.send(msg);
-	match res {
-	    Ok(bsend) => assert_eq!(bsend, 11),
-	    Err(fail) => panic!(fail),
-	}
+        let res = ns.send(msg);
+        match res {
+            Ok(bsend) => assert_eq!(bsend, 11),
+            Err(fail) => panic!(fail),
+        }
     }
 }
